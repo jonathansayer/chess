aaee4806f7f7c2a515f638845bc379f6f7e6d379
jonathansayer <jon.sayer1@gmail.com>
2015-09-26 18:59:36 +0100

    refactored rook

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,17 +1,15 @@
 class Rook < ActiveRecord::Base
 
-  def move_vertically new_position
-    current_coords = ConvertCoordinates.convert_to_numercal_coords position
-    new_coords = ConvertCoordinates.convert_to_numercal_coords new_position
-    raise "Invalid Move" if new_coords[0] != current_coords[0]
+  def move_to new_position
+    @current_coords = ConvertCoordinates.convert_to_numercal_coords position
+    @new_coords = ConvertCoordinates.convert_to_numercal_coords new_position
+    check_if_not_a_valid_move
     self.position = new_position
   end
 
-  def move_horizontally new_position
-    current_coords = ConvertCoordinates.convert_to_numercal_coords position
-    new_coords = ConvertCoordinates.convert_to_numercal_coords new_position
-    raise "Invalid Move" if new_coords[1] != current_coords[1]
-    self.position = new_position
-  end
+  private
 
+  def check_if_not_a_valid_move
+    raise "Invalid Move" if @new_coords[0] != @current_coords[0] and @new_coords[1] != @current_coords[1]
+  end
 end
--- spec/models/rook_spec.rb
+++ spec/models/rook_spec.rb
@@ -8,52 +8,44 @@ describe Rook do
     expect(subject).to respond_to :position
   end
 
-  it 'should respond to move_vertically with one arguement' do
-    expect(subject).to respond_to(:move_vertically).with(1).argument
+  it 'should respond to move with one arguement' do
+    expect(subject).to respond_to(:move_to).with(1).argument
   end
 
   it 'should be able to move forward one cell' do
-    subject.move_vertically 'A2'
+    subject.move_to 'A2'
     expect(subject.position).to eq 'A2'
   end
 
   it 'should be able to move forward more than one cell' do
-    subject.move_vertically 'A8'
+    subject.move_to 'A8'
     expect(subject.position).to eq 'A8'
   end
 
   it 'should be able to move backward one space' do
     subject.position = 'A8'
-    subject.move_vertically 'A7'
+    subject.move_to 'A7'
     expect(subject.position).to eq 'A7'
   end
 
   it 'should be able to move backward more than one space' do
     subject.position = 'A8'
-    subject.move_vertically 'A1'
+    subject.move_to 'A1'
     expect(subject.position).to eq 'A1'
   end
 
   it 'should not be able to move diagonally' do
-    expect{subject.move_vertically 'B2'}.to raise_error "Invalid Move"
-  end
-
-  it 'should be able to move horizontally with one argument' do
-    expect(subject).to respond_to(:move_horizontally).with(1).argument
+    expect{subject.move_to 'B2'}.to raise_error "Invalid Move"
   end
 
   it 'should be able to move horizontally one cell' do
-    subject.move_horizontally 'B1'
+    subject.move_to 'B1'
     expect(subject.position).to eq 'B1'
   end
 
   it 'should be able to move horizontally multiple cells' do
-    subject.move_horizontally 'H1'
+    subject.move_to 'H1'
     expect(subject.position).to eq 'H1'
   end
-
-  it 'should not be allowd to move diagonally using horizontal method' do
-    expect{subject.move_horizontally 'B2'}.to raise_error "Invalid Move"
-  end 
 
 end
