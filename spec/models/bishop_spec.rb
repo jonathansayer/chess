555e57ed6b63fc7910eabd2793c88ad7cf7a569b
jonathansayer <jon.sayer1@gmail.com>
2015-09-27 11:35:10 +0100

    Bishop can move by one in any diagonal direction

--- spec/models/bishop_spec.rb
+++ spec/models/bishop_spec.rb
@@ -12,11 +12,30 @@ describe Bishop do
 
   context "when moving diagonally" do
 
-    it 'should be able to move diagonally forward-right by 1(from D4 to E5)' do
+    before(:each) do
       subject.position = 'D4'
+    end
+
+    it 'should be able to move diagonally forward-right by 1(from D4 to E5)' do
       subject.move_to 'E5'
       expect(subject.position).to eq 'E5'
     end
+
+    it 'should be able to move diagonally forward-left by 1(from D4 to C5)' do
+      subject.move_to 'C5'
+      expect(subject.position).to eq 'C5'
+    end
+
+    it 'should be able to move diagonally backwards-right by 1(from D4 to E3)' do
+      subject.move_to 'E3'
+      expect(subject.position).to eq 'E3'
+    end
+
+    it 'should be able to move diagonally backwwards-left by 1(from D4 to C3)' do
+      subject.move_to 'C3'
+      expect(subject.position).to eq 'C3'
+    end
+
   end
 
 end
