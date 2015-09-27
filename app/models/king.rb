928c9bfd8341e16f89ce9a2904b2b2cfc18437ba
jonathansayer <jon.sayer1@gmail.com>
2015-09-27 16:38:15 +0100

    King can move by 1 in all directions

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,2 +1,35 @@
 class King < ActiveRecord::Base
+
+  def move_to new_position
+    raise "Invalid Move" unless horizonatal_move? new_position or vertical_move? new_position or diagonal_move? new_position
+    self.position = new_position
+  end
+
+  private
+
+  def horizonatal_move? new_position
+      current_coords = ConvertCoordinates.convert_to_numercal_coords self.position
+      new_coords = ConvertCoordinates.convert_to_numercal_coords new_position
+      return false if (new_coords[0] - current_coords[0]).abs > move_limit
+      return new_coords[1] == current_coords[1]
+  end
+
+  def vertical_move? new_position
+    current_coords = ConvertCoordinates.convert_to_numercal_coords self.position
+    new_coords = ConvertCoordinates.convert_to_numercal_coords new_position
+    return false if (new_coords[1] - current_coords[1]).abs > move_limit
+    return new_coords[0] == current_coords[0]
+  end
+
+  def diagonal_move? new_position
+    current_coords = ConvertCoordinates.convert_to_numercal_coords self.position
+    new_coords = ConvertCoordinates.convert_to_numercal_coords new_position
+    return false if (new_coords[0] - current_coords[0]).abs > 1 or (new_coords[1] - current_coords[1]).abs > 1
+    return ((current_coords[0] - new_coords[0]).abs == (current_coords[1] - new_coords[1]).abs)
+  end
+
+  def move_limit
+    1
+  end
+
 end
--- spec/models/king_spec.rb
+++ spec/models/king_spec.rb
@@ -1,4 +1,95 @@
 require 'rails_helper'
 
 describe King do
+
+  before(:each) do
+    subject.position = 'D4'
+  end
+
+  it 'should respond to position' do
+    expect(subject).to respond_to :position
+  end
+
+  it 'should respond to move_to with one arguement' do
+    expect(subject).to respond_to(:move_to).with(1).argument
+  end
+
+  it 'should be able to move forward by from D4 to D5' do
+    subject.move_to 'D5'
+    expect(subject.position).to eq 'D5'
+  end
+
+  it 'should be able to move backwards by one' do
+    subject.move_to "D3"
+    expect(subject.position).to eq "D3"
+  end
+
+  it 'should be able to move right by one' do
+    subject.move_to "E4"
+    expect(subject.position).to eq "E4"
+  end
+
+  it 'should be able to move left by one' do
+    subject.move_to "D4"
+    expect(subject.position).to eq "D4"
+  end
+
+  it 'should be able to move diagonally forward-right by one(from D4 to E5)' do
+    subject.move_to "E5"
+    expect(subject.position).to eq "E5"
+  end
+
+  it 'should be able to move diagonally forward-left by one(from D4 to C5)' do
+    subject.move_to "C5"
+    expect(subject.position).to eq "C5"
+  end
+
+  it 'should be able to move diagonally backward-right by one(from D4 to E3)' do
+    subject.move_to "E3"
+    expect(subject.position).to eq "E3"
+  end
+
+  it 'should be able to move diagonally backward-left by one(from D4 to C3)' do
+    subject.move_to "C3"
+    expect(subject.position).to eq "C3"
+  end
+
+  context "when trying to make an invalid move" do
+
+    it 'should not be able to move to E6 from D4' do
+      expect{subject.move_to 'E6'}.to raise_error "Invalid Move"
+    end
+
+    it 'cannot move more than one place forward ( from D4 to D6 )' do
+      expect{subject.move_to 'D6'}.to raise_error "Invalid Move"
+    end
+
+    it 'cannot move more than one place backward (from D4 to D2)' do
+      expect{subject.move_to 'D2'}.to raise_error "Invalid Move"
+    end
+
+    it 'cannot move more than one place right (from D4 to F4)' do
+      expect{subject.move_to 'F4'}.to raise_error "Invalid Move"
+    end
+
+    it 'cannot move more than one place left (from D4 to B4)' do
+      expect{subject.move_to 'B4'}.to raise_error "Invalid Move"
+    end
+
+    it 'cannot more more than one place diagonally forward-right (from D4 to F6)' do
+      expect{subject.move_to 'F6'}.to raise_error "Invalid Move"
+    end
+
+    it 'cannot more more than one place diagonally forward-left (from D4 to B6)' do
+      expect{subject.move_to 'B6'}.to raise_error "Invalid Move"
+    end
+
+    it 'cannot more more than one place diagonally backward-right (from D4 to F2)' do
+      expect{subject.move_to 'F2'}.to raise_error "Invalid Move"
+    end
+
+    it 'cannot more more than one place diagonally backward-left (from D4 to B2)' do
+      expect{subject.move_to 'B2'}.to raise_error "Invalid Move"
+    end
+  end
 end
