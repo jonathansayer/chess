a7d0ee02bafa04c6f5be07369ebf44a595dd04b5
jonathansayer <jon.sayer1@gmail.com>
2015-09-27 12:03:40 +0100

    refactored Bishop class

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -1,10 +1,20 @@
 class Bishop < ActiveRecord::Base
 
   def move_to new_position
+    raise 'Invalid Move' if invalid_move? new_position
+    self.position = new_position
+  end
+
+  private
+
+  def invalid_move? new_position
     current_coords = ConvertCoordinates.convert_to_numercal_coords self.position
     new_coords = ConvertCoordinates.convert_to_numercal_coords new_position
-    raise 'Invalid Move' if ((current_coords[0] - new_coords[0]).abs != (current_coords[1] - new_coords[1]).abs)
-    self.position = new_position
+    if ((current_coords[0] - new_coords[0]).abs != (current_coords[1] - new_coords[1]).abs)
+      return true
+    else
+      return false
+    end
   end
 
 end
