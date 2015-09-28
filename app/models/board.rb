057757cc5b365d9748571c4d37d9dc06beb868bf
jonathansayer <jon.sayer1@gmail.com>
2015-09-28 16:55:00 +0100

    refactored board.rb

--- app/models/board.rb
+++ app/models/board.rb
@@ -10,8 +10,18 @@ class Board < ActiveRecord::Base
   def move_piece piece, new_position
     old_position = piece.position
     piece.move_to new_position
+    occupy_cell_at new_position
+    leave_cell_at old_position
+  end
+
+  private
+
+  def occupy_cell_at new_position
     to_cell = Cell.find_by(position: new_position)
     to_cell.change_occupied_mode
+  end
+
+  def leave_cell_at old_position
     from_cell = Cell.find_by(position: old_position)
     from_cell.change_occupied_mode
   end
