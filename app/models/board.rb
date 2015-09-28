5e0a3dc40cbf710079823c90f575a01151384c23
jonathansayer <jon.sayer1@gmail.com>
2015-09-28 21:40:10 +0100

    a piece can take another of the board

--- app/models/board.rb
+++ app/models/board.rb
@@ -18,12 +18,30 @@ class Board < ActiveRecord::Base
 
   def occupy_cell_at new_position
     to_cell = Cell.find_by(position: new_position)
-    to_cell.change_occupied_mode
+    to_cell.change_occupied_mode if to_cell.occupied? == false
+    remove_piece new_position if to_cell.occupied? == true
   end
 
   def leave_cell_at old_position
     from_cell = Cell.find_by(position: old_position)
     from_cell.change_occupied_mode
+  end
+
+  def remove_piece new_position
+    if Pawn.exists?(position: new_position)
+      removed_piece = Pawn.where(position: new_position).first
+    elsif Rook.exists?(position: new_position)
+      removed_piece = Rook.where(position: new_position).first
+    elsif Bishop.exists?(position: new_position)
+      removed_piece = Bishop.where(position: new_position).first
+    elsif Knight.exists?(position: new_position)
+      removed_piece = Knight.where(position: new_position).first
+    elsif Queen.exists?(position: new_position)
+      removed_piece = Queen.where(position: new_position).first
+    elsif King.exists?(position: new_position)
+      removed_piece = King.where(position: new_position).first
+    end
+    removed_piece.update_column("position", "Off Board")
   end
 
 end
--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -8,34 +8,76 @@ describe Board do
 
   before(:each) do
     allow(pawn).to receive(:position) {'D4'}
+    allow(to_cell).to receive(:occupied?) {false}
     cell_class = class_double('Cell').as_stubbed_const(:transfer_nested_constants => true)
     allow(cell_class).to receive(:find_by).with({:position=>"D5"}){to_cell}
     allow(cell_class).to receive(:find_by).with({:position=>"D4"}){from_cell}
   end
 
-  it 'should respond to move_piece with two arguments' do
-    expect(subject).to respond_to(:move_piece).with(2).argument
+  context 'When moving pieces' do
+
+    it 'should respond to move_piece with two arguments' do
+      expect(subject).to respond_to(:move_piece).with(2).argument
+    end
+
+    it 'should call the move_to method for the piece' do
+      allow(from_cell).to receive(:change_occupied_mode)
+      allow(to_cell).to receive(:change_occupied_mode)
+      expect(pawn).to receive(:move_to)
+      subject.move_piece pawn, 'D5'
+    end
+
+    it 'should change the occupied state of the Cell that the piece moves to' do
+      allow(pawn).to receive(:move_to)
+      allow(from_cell).to receive(:change_occupied_mode)
+      expect(to_cell).to receive(:change_occupied_mode)
+      subject.move_piece pawn, 'D5'
+    end
+
+    it 'should change the occupied state of the Cell that the piece move away from' do
+      allow(pawn).to receive(:move_to)
+      allow(to_cell).to receive(:change_occupied_mode)
+      expect(from_cell).to receive(:change_occupied_mode)
+      subject.move_piece pawn, 'D5'
+    end
   end
 
-  it 'should call the move_to method for the piece' do
-    allow(from_cell).to receive(:change_occupied_mode)
-    allow(to_cell).to receive(:change_occupied_mode)
-    expect(pawn).to receive(:move_to)
-    subject.move_piece pawn, 'D5'
+  context 'when taking pieces' do
+
+    it 'should be able to move to a cell which is already occupied and cell shoudl still be occupied' do
+      to_cell = double :to_cell, occupied?: true
+      pawn2 = double :pawn2, position: 'E5'
+      allow(pawn).to receive(:move_to)
+      allow(from_cell).to receive(:change_occupied_mode)
+      cell_class = class_double('Cell').as_stubbed_const(:transfer_nested_constants => true)
+      allow(cell_class).to receive(:find_by).with({:position=>"E5"}){to_cell}
+      allow(cell_class).to receive(:find_by).with({:position=>"D4"}){from_cell}
+      pawn_class = class_double('Pawn').as_stubbed_const(:transfer_nested_constants => true)
+      allow(pawn_class).to receive(:exists?).with({:position=>"E5"}){true}
+      allow(pawn_class).to receive(:where).with({:position=>"E5"}){[pawn2]}
+      allow(pawn2).to receive(:update_column).with("position", "Off Board")
+      expect(to_cell).not_to receive(:change_occupied_mode)
+      subject.move_piece pawn, 'E5'
+    end
+
+    it 'should be able to move a piece off that board when another piece moves to that cell' do
+      allow(pawn).to receive(:move_to)
+      pawn2 = double :pawn2, position: 'E5'
+      to_cell = double :to_cell, occupied?: true, position: 'E5'
+      pawn_class = class_double('Pawn').as_stubbed_const(:transfer_nested_constants => true)
+      allow(pawn_class).to receive(:exists?).with({:position=>"E5"}){true}
+      allow(pawn_class).to receive(:where).with({:position=>"E5"}){[pawn2]}
+      cell_class = class_double('Cell').as_stubbed_const(:transfer_nested_constants => true)
+      allow(cell_class).to receive(:find_by).with({:position=>"E5"}){to_cell}
+      allow(cell_class).to receive(:find_by).with({:position=>"D4"}){from_cell}
+      allow(from_cell).to receive(:change_occupied_mode)
+      expect(pawn2).to receive(:update_column).with("position", "Off Board")
+      subject.move_piece pawn, 'E5'
+    end
+
   end
 
-  it 'should change the occupied state of the Cell that the piece moves to' do
-    allow(pawn).to receive(:move_to)
-    allow(from_cell).to receive(:change_occupied_mode)
-    expect(to_cell).to receive(:change_occupied_mode)
-    subject.move_piece pawn, 'D5'
-  end
 
-  it 'should change the occupied state of the Cell that the piece move away from' do
-    allow(pawn).to receive(:move_to)
-    allow(to_cell).to receive(:change_occupied_mode)
-    expect(from_cell).to receive(:change_occupied_mode)
-    subject.move_piece pawn, 'D5'
-  end
+
 
 end
