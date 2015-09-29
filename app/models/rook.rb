d40cf72bfe7270a5f8e25e0c449eef13b8b7343c
jonathansayer <jon.sayer1@gmail.com>
2015-09-29 18:12:05 +0100

    refactored rook

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -22,17 +22,29 @@ class Rook < ActiveRecord::Base
   end
 
   def piece_in_path? new_position
+    return true if piece_in_the_y_range? new_position and vertical_move? new_position
+    return true if piece_in_the_x_range? new_position and horizonatal_move? new_position
+    return false
+  end
+
+  def piece_in_the_y_range? new_position
     path_coords = ConvertCoordinates.convert_to_numercal_coords self.position, new_position
     y_range = ((path_coords.first[1]+1)...path_coords.last[1])
     y_range.each do |y_coord|
       path_position = ConvertCoordinates.convert_to_alphabetical_coords [path_coords.first[0],y_coord]
       return true if Cell.find_by(position: path_position).occupied?
     end
+    return false
+  end
+
+  def piece_in_the_x_range? new_position
+    path_coords = ConvertCoordinates.convert_to_numercal_coords self.position, new_position
     x_range = ((path_coords.first[0]+1)...path_coords.last[0])
     x_range.each do |x_coord|
       path_position = ConvertCoordinates.convert_to_alphabetical_coords [x_coord,path_coords.first[1]]
       return true if Cell.find_by(position: path_position).occupied?
-    end 
+    end
     return false
   end
+
 end
