835722ad1bc94d12144a1abea30dbe4d6481c803
jonathansayer <jon.sayer1@gmail.com>
2015-10-01 17:26:28 +0100

    refactored queen

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -16,21 +16,23 @@ class Queen < ActiveRecord::Base
   private
 
     def horizonatal_move?
-        current_coords = ConvertCoordinates.convert_to_numercal_coords self.position
-        new_coords = ConvertCoordinates.convert_to_numercal_coords @new_position
         return new_coords[1] == current_coords[1]
     end
 
     def vertical_move?
-      current_coords = ConvertCoordinates.convert_to_numercal_coords self.position
-      new_coords = ConvertCoordinates.convert_to_numercal_coords @new_position
       return new_coords[0] == current_coords[0]
     end
 
     def diagonal_move?
-      current_coords = ConvertCoordinates.convert_to_numercal_coords self.position
-      new_coords = ConvertCoordinates.convert_to_numercal_coords @new_position
       return ((current_coords[0] - new_coords[0]).abs == (current_coords[1] - new_coords[1]).abs)
+    end
+
+    def current_coords
+      current_coords = ConvertCoordinates.convert_to_numercal_coords self.position
+    end
+
+    def new_coords
+      new_coords = ConvertCoordinates.convert_to_numercal_coords @new_position
     end
 
     def piece_in_path?
@@ -41,9 +43,8 @@ class Queen < ActiveRecord::Base
     end
 
     def piece_in_the_y_range?
-      piece_coords_path = ConvertCoordinates.convert_to_numercal_coords self.position, @new_position
       y_range.each do |y_coord|
-        path_position = ConvertCoordinates.convert_to_alphabetical_coords [piece_coords_path.first[0],y_coord]
+        path_position = ConvertCoordinates.convert_to_alphabetical_coords [current_coords[0],y_coord]
         if path_position != self.position and path_position != @new_position
           return true if Cell.find_by(position: path_position).occupied?
         end
@@ -52,9 +53,8 @@ class Queen < ActiveRecord::Base
     end
 
     def piece_in_the_x_range?
-      piece_coords_path = ConvertCoordinates.convert_to_numercal_coords self.position, @new_position
       x_range.each do |x_coord|
-        path_position = ConvertCoordinates.convert_to_alphabetical_coords [x_coord,piece_coords_path.first[1]]
+        path_position = ConvertCoordinates.convert_to_alphabetical_coords [x_coord,current_coords[1]]
         if path_position != self.position and path_position != @new_position
           return true if Cell.find_by(position: path_position).occupied?
         end
@@ -77,19 +77,18 @@ class Queen < ActiveRecord::Base
     end
 
     def x_range
-      piece_coords_path = ConvertCoordinates.convert_to_numercal_coords self.position, @new_position
-      x_coordinates_in_path = ((piece_coords_path.first[0])..piece_coords_path.last[0]).to_a
-      if piece_coords_path.first[0] > piece_coords_path.last[0]
-        x_coordinates_in_path = piece_coords_path.first[0].downto(piece_coords_path.last[0]).to_a
+      x_coordinates_in_path = ((current_coords[0])..new_coords[0]).to_a
+      if current_coords[0] > new_coords[0]
+        x_coordinates_in_path = current_coords[0].downto(new_coords[0]).to_a
       end
       return x_coordinates_in_path
     end
 
     def y_range
-      piece_coords_path = ConvertCoordinates.convert_to_numercal_coords self.position, @new_position
-      y_coordinates_in_path = ((piece_coords_path.first[1])..piece_coords_path.last[1]).to_a
-      if piece_coords_path.first[1] > piece_coords_path.last[1]
-        y_coordinates_in_path = piece_coords_path.first[1].downto(piece_coords_path.last[1]).to_a
+      piece_coords_path = [current_coords, new_coords]
+      y_coordinates_in_path = (current_coords[1]..new_coords[1]).to_a
+      if current_coords[1] > new_coords[1]
+        y_coordinates_in_path = current_coords[1].downto(new_coords[1]).to_a
       end
       return y_coordinates_in_path
     end
