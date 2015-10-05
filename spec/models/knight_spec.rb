fb8873a665170f44c7f1bf6e77f8b1898549a31a
jonathansayer <jon.sayer1@gmail.com>
2015-10-05 10:17:13 +0100

    refactored knight spec

--- spec/models/knight_spec.rb
+++ spec/models/knight_spec.rb
@@ -4,6 +4,14 @@ describe Knight do
 
   before(:each) do
     subject.position = 'D4'
+    convert_class = class_double('ConvertCoordinates').as_stubbed_const(:transfer_nested_constants => true)
+    allow(convert_class).to receive(:to_numerical_coords).with('B3'){[2,3]}
+    allow(convert_class).to receive(:to_numerical_coords).with('B5'){[2,5]}
+    allow(convert_class).to receive(:to_numerical_coords).with('C6'){[3,6]}
+    allow(convert_class).to receive(:to_numerical_coords).with('D4'){[4,4]}
+    allow(convert_class).to receive(:to_numerical_coords).with('F5'){[6,5]}
+    allow(convert_class).to receive(:to_numerical_coords).with('E4'){[5,4]}
+    allow(convert_class).to receive(:to_numerical_coords).with('E6'){[5,6]}
   end
 
   it 'should respond to position' do
@@ -15,41 +23,26 @@ describe Knight do
   end
 
   it 'should be able to move from D4 to E6' do
-    convert_class = class_double('ConvertCoordinates').as_stubbed_const(:transfer_nested_constants => true)
-    allow(convert_class).to receive(:to_numerical_coords).with('D4'){[4,4]}
-    allow(convert_class).to receive(:to_numerical_coords).with('E6'){[5,6]}
     subject.move_to 'E6'
     expect(subject.position).to eq 'E6'
   end
 
   it 'should be able to move from D4 to F5' do
-    convert_class = class_double('ConvertCoordinates').as_stubbed_const(:transfer_nested_constants => true)
-    allow(convert_class).to receive(:to_numerical_coords).with('D4'){[4,4]}
-    allow(convert_class).to receive(:to_numerical_coords).with('F5'){[6,5]}
     subject.move_to 'F5'
     expect(subject.position).to eq 'F5'
   end
 
   it 'should be able to move from D4 tp B3' do
-    convert_class = class_double('ConvertCoordinates').as_stubbed_const(:transfer_nested_constants => true)
-    allow(convert_class).to receive(:to_numerical_coords).with('D4'){[4,4]}
-    allow(convert_class).to receive(:to_numerical_coords).with('B3'){[2,3]}
     subject.move_to 'B3'
     expect(subject.position).to eq 'B3'
   end
 
   it 'should be able to move from D4 to B5' do
-    convert_class = class_double('ConvertCoordinates').as_stubbed_const(:transfer_nested_constants => true)
-    allow(convert_class).to receive(:to_numerical_coords).with('D4'){[4,4]}
-    allow(convert_class).to receive(:to_numerical_coords).with('B5'){[2,5]}
     subject.move_to 'B5'
     expect(subject.position).to eq 'B5'
   end
 
   it 'should be able to move from D4 to C6' do
-    convert_class = class_double('ConvertCoordinates').as_stubbed_const(:transfer_nested_constants => true)
-    allow(convert_class).to receive(:to_numerical_coords).with('D4'){[4,4]}
-    allow(convert_class).to receive(:to_numerical_coords).with('C6'){[3,6]}
     subject.move_to 'C6'
     expect(subject.position).to eq 'C6'
   end
@@ -57,9 +50,6 @@ describe Knight do
   context "when trying to make an invalid move" do
 
     it 'should not be able to move from D4 to E4' do
-      convert_class = class_double('ConvertCoordinates').as_stubbed_const(:transfer_nested_constants => true)
-      allow(convert_class).to receive(:to_numerical_coords).with('D4'){[4,4]}
-      allow(convert_class).to receive(:to_numerical_coords).with('E4'){[5,4]}
       expect{subject.move_to 'E4'}.to raise_error 'Invalid Move'
     end
   end
