532d8c78c9525d375426d2c390250bee24561914
jonathansayer <jon.sayer1@gmail.com>
2015-10-06 20:07:51 +0100

    created player

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150930142917) do
+ActiveRecord::Schema.define(version: 20151006190629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,11 @@ ActiveRecord::Schema.define(version: 20150930142917) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean  "white?"
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "queens", force: :cascade do |t|
